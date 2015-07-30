bdda68f9224b596656d1af3806c75d996c07c4c9
EJLarcs <ejlarcombe@gmail.com>
2015-07-30 17:57:11 -0400

    conference

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map { | name | "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  array = []
+  speakers.each_with_index do |speaker, index| 
+    array << "Hello, #{speaker}! You'll be assigned to room #{index+1}!" 
+  end
+  array
+end 
+
+def printer(speakers)
+  to_print1 = []
+  speakers.map { | name | to_print1 << "Hello, my name is #{name}."}
+  to_print2 = []
+  speakers.each_with_index { |speaker, index| 
+    to_print2 << "Hello, #{speaker}! You'll be assigned to room #{index+1}!" 
+  }
+  to_print1.each { |badge| puts badge }
+  to_print2.each { |room| puts room }
+end
+
